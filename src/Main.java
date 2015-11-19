e68c503689b08a9fcd8cbd7e83857a54b65d1047
Nakamura <Nakamura@Nakamuras-MacBook-Pro.local>
2015-11-19 13:06:10 +0800

    modified main

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,7 @@ public class Main {
             System.out.println(rs.getString(1));
         }
 
+        System.out.println("hihihi");
         //close
         rs.close();
         stmt.close();
