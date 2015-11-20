3ed8e78afec3ca376afdfea1cf8646ad1cb92861
nakamurameijin <nakamurameijin@gmail.com>
2015-11-21 01:42:46 +0800

    add a debug line

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
         Class.forName("oracle.jdbc.driver.OracleDriver");
         Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@db12.cse.cuhk.edu.hk:1521:db12", "d103", "123456789");
 
+        Administrator.menu(); //debugging
+
         //Query
         PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM fuser WHERE name LIKE ?");
         pstmt.setString(1,"%Terence%");
